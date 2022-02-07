b0c6b2d93991a5d2bc563022694441b6e6d94dd5
bombvvv <bombardieri.manuele.studente@itispaleocapa.it>
2022-02-07 10:27:28 +0000

    change

--- src/php/signIn.php
+++ src/php/signIn.php
@@ -23,7 +23,7 @@ function main(){
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   main();
 }else{
-echo str_replace("<!-- cssPos -->",file_get_contents("/css/main.css"),file_get_contents("/html/signin.html"));
+echo str_replace("<!-- cssPos -->",file_get_contents("css/main.css"),file_get_contents("html/signin.html"));
 }
 
 ?>
