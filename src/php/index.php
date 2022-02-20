400a28aee58a46c8322201bff3cd03d588efaf47
bombvvv <bombardieri.manuele.studente@itispaleocapa.it>
2022-02-20 16:04:34 +0000

    ciao

--- src/php/index.php
+++ src/php/index.php
@@ -1,19 +1,35 @@
 <?php
+$userData = array(
+	"pippo@coso.com"=>"test",
+);
 
-function showError($errorCode, $errorName, $errorDesc){
-    $responseObj = new stdClass();
-    http_response_code($errorCode);
-    header('Content-Type: application/json; charset=utf-8');
-    echo "{\"$errorName\":\"$errorDesc\"}";
-}
 function main(){
-    echo "yo bro";
-
+	global $userData;
+    $email = $_POST['email'];
+    $classId = "5id";
+    $pass = $_POST['pass'] . $classId;
+    $pass = hash('SHA256',($pass));
+    $conn = mysqli_connect(storage, bombv, ciao76 , $dbname);
+    if (isset($email)) {
+      if (isset($pass)) {
+        /*
+        $fp = fopen('data.txt', 'a');//opens file in append mode.
+        fwrite($fp, "\n".$email.';'.$pass.';');
+        fclose($fp);
+        */
+        
+		    echo str_replace("/*cssPos*/",file_get_contents("css/loginSignin.css"),file_get_contents("html/login.html"));
+        return;
+      }
+    }
+    echo var_dump($userData);
+    echo " nop";
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    main();
-  }else{
-      echo str_replace("/*cssPos*/",file_get_contents("css/main.css"),file_get_contents("html/index.html"));
-  }
+  main();
+}else{
+echo str_replace("/*cssPos*/",file_get_contents("css/loginSignin.css"),file_get_contents("html/index.html"));
+}
+
 ?>
---
+++ src/php/site.php
@@ -0,0 +1,19 @@
+<?php
+
+function showError($errorCode, $errorName, $errorDesc){
+    $responseObj = new stdClass();
+    http_response_code($errorCode);
+    header('Content-Type: application/json; charset=utf-8');
+    echo "{\"$errorName\":\"$errorDesc\"}";
+}
+function main(){
+    
+
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    main();
+  }else{
+      echo str_replace("/*cssPos*/",file_get_contents("css/site.css"),file_get_contents("html/site.html"));
+  }
+?>
